fa64c97af365572e837c97b88ecf6b284d2c6bca
Manuki <robercraudio@gmail.com>
2020-11-05 05:47:02 -0300

    "validando a data" finished

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentSameDate) {
+    return response.status(400).json({ message: 'Duplicated appointment!' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes';
+
+const port = 3333;
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Serv started on port: ${port}`);
+});
+
+// test
