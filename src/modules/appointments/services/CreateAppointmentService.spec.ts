e01bf238ca6af2dab9d0e57f0e1305db62447dd0
Manuki <robercraudio@gmail.com>
2020-12-28 21:27:25 -0300

    "pensando nos testes" finished

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,13 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+// Testes unitários ==> sem dependência externa ==> Fake repository
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+
+  })
+
+  // it('should not be able to create two appointments in the same date', () => {
+
+  // })
+})
