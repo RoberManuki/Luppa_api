70b66c2a5ac8f1d8b07978d2fa29672a9f16de8c
Manuki <robercraudio@gmail.com>
2021-01-05 00:12:24 -0300

    "aplicando TDD na prática" finished

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,26 @@
+// import User from '@modules/users/infra/typeorm/entities/User';
+// import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    this.mailProvider.sendMail(email, 'email recover body');
+  }
+}
+
+export default SendForgotPasswordEmailService;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>(
+//   'MailProvider',
+//   DiskStorageProvider,
+// );
